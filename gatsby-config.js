ccd91b046dc9a308ede8ff1ee2c476db639c6b9d
Kiran U Kamath <32927230+kirankamatmgm@users.noreply.github.com>
2020-04-12 07:54:30 +0530

    Title change

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Kiran Kamath`,
+    title: `Kiran U Kamath`,
     author: `Kiran U Kamath`,
     description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
