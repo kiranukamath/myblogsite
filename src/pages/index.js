d002175a981f3b33f55f6d7cdf8c815ddabec778
Kiran U Kamath <32927230+kirankamatmgm@users.noreply.github.com>
2020-05-13 14:22:05 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,15 +28,13 @@ class IndexPage extends React.Component {
         <p>Welcome to my Personal Website</p>
         <p>
           I am currently pursuing Information science and Engineering in NIE mysore. Interested in machine learing.
-        To look at my projects check <a href="https://github.com/kirankamatmgm">Github</a>
+        To look at my projects check <a class="github-button" href="https://github.com/kirankamatmgm" data-color-scheme="no-preference: dark; light: dark; dark: dark;" data-size="large" aria-label="Follow @kirankamatmgm on GitHub">Follow @kirankamatmgm</a><script async defer src="https://buttons.github.io/buttons.js"></script>
         </p><br />
         <p>You can contact me through<br />
           email: kirankamat.mgm@gmail.com<br />
           or follow me on &nbsp;
         </p>
 
-
-        <a class="github-button" href="https://github.com/kirankamatmgm" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" aria-label="Follow @kirankamatmgm on GitHub">Follow @kirankamatmgm</a><script async defer src="https://buttons.github.io/buttons.js"></script>
 
         <div class="LI-profile-badge"  data-version="v1" data-size="large" data-locale="en_US" data-type="horizontal" data-theme="light" data-vanity="kiran-u-kamath"><a class="LI-simple-link" href='https://in.linkedin.com/in/kiran-u-kamath?trk=profile-badge'>Kiran U Kamath</a></div><script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
 
